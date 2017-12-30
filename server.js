582563d25c97de1663dda123896c0f750dd3a3fe
floune <robin.sangla@live.fr>
2017-12-30 15:27:21 +0100

    commit01 back: node + express + route cheerio qui va chercher une liste sur reelgood, front: react + console.log de la liste

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const request = require('request');
+const cheerio = require('cheerio');
+
+app.get('/api/scrap', function(req, res) {
+	url = 'https://reelgood.com/movies/genre/comedy';
+	request(url, function(error, response, html) {
+		if (!error) {
+			let $ = cheerio.load(html);
+			let title;
+			let json = {titles: []};
+			let lon;
+			$('.a8').filter(function() {
+				let data = $(this);
+				data.children().each(function(i){
+					if ($(this).text() !== '')
+						json.titles.push($(this).text());
+				})
+			})
+			res.send({json});
+		}
+	})
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`));
