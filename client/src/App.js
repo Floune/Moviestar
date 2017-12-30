4a46f1e75d0f70ca60a690f1ee3830ed983c3f95
floune <robin.sangla@live.fr>
2017-12-30 15:50:41 +0100

    titres tournants

--- client/src/App.js
+++ client/src/App.js
@@ -28,9 +28,9 @@ class App extends Component {
       <div className="App">
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-logo">Welcome to React</h1>
       </header>
-      <p className="App-intro">
+      <p className="App-logo">
       wesh
       </p>
       </div>
